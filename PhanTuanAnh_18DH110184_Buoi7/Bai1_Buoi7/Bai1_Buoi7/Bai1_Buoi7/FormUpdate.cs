2c951566b598e2b12f3e3ebe5b117def38c92f53
PhanTuanAnh1811 <49008994+PhanTuanAnh1811@users.noreply.github.com>
2020-05-20 21:53:32 +0700

    PhanTuanAnh_18DH110184_Buoi7

---
+++ PhanTuanAnh_18DH110184_Buoi7/Bai1_Buoi7/Bai1_Buoi7/Bai1_Buoi7/FormUpdate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1_Buoi7
+{
+    public partial class FormUpdate : Form
+    {
+        public FormUpdate()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUpdate_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            Form1 F1 = new Form1();
+            F1.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mshh = txtMSHH.Text;
+            string tenSanPham = txtTenSanPham.Text;
+            int gia = int.Parse(txtGia.Text);
+            bool tinhTrang = radioConHang.Checked ? true : false;
+
+           // DataProvider data = new DataProvider();
+            //data.ThemSanPham(mshh, tenSanPham, gia, tinhTrang);
+
+            MessageBox.Show("Bạn đã thêm thành công", "OKE!");
+            Clear();
+        }
+
+        public void Clear()
+        {
+            txtMSHH.Text = "";
+            txtGia.Text = "";
+            txtTenSanPham.Text = "";
+            radioConHang.Checked = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+    }
+}
